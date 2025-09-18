439e6847656cb0eda50e2813a01abe25f01f990d
GeonwooKim <scatteringbrain@gmail.com>
2025-09-18 12:55:22 -0500

    1

---
+++ work1/build/CMakeFiles/solenoid1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/gkim2/working/work1/solenoid1.cc" "CMakeFiles/solenoid1.dir/solenoid1.cc.o" "gcc" "CMakeFiles/solenoid1.dir/solenoid1.cc.o.d"
+  "/Users/gkim2/working/work1/src/ActionInitialization.cc" "CMakeFiles/solenoid1.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/solenoid1.dir/src/ActionInitialization.cc.o.d"
+  "/Users/gkim2/working/work1/src/DetectorConstruction.cc" "CMakeFiles/solenoid1.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/solenoid1.dir/src/DetectorConstruction.cc.o.d"
+  "/Users/gkim2/working/work1/src/PrimaryGeneratorAction.cc" "CMakeFiles/solenoid1.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/solenoid1.dir/src/PrimaryGeneratorAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ work1/build/CMakeFiles/solenoid1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solenoid1.dir/solenoid1.cc.o"
+  "CMakeFiles/solenoid1.dir/solenoid1.cc.o.d"
+  "CMakeFiles/solenoid1.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/solenoid1.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/solenoid1.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/solenoid1.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/solenoid1.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/solenoid1.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "solenoid1"
+  "solenoid1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solenoid1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
